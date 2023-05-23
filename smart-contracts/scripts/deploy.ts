26bf30f112f92ccd85b5535bb30fa418c6ce7377
Marko <marko.ivankovic650@gmail.com>
2023-05-23 20:37:04 +0200

    add deloyment script

--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,24 +1,20 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+type DeployArgs = [_tokenSupply: string, _maxSupply: string, _baseURI: string];
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+const deploy = async () => {
+  const deployArgs: DeployArgs = [
+    "0.01",
+    "6",
+    "ipfs://QmesdkpFTZF96SEa8ppPCkP2aibiE9yqJ6WBPi5XLjb6kS/",
+  ];
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const Fellas = await ethers.getContractFactory("Fellas");
+  const fellas = await Fellas.deploy(...deployArgs);
 
-  await lock.deployed();
+  await fellas.deployed();
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
+  console.log(`Fellas deployed at: ${fellas.address}`);
+};
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+deploy();
