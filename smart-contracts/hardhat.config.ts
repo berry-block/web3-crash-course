75524238a3e045e7326a125a31134473c27d2ed8
Marko <marko.ivankovic650@gmail.com>
2023-05-23 20:50:19 +0200

    setup deployment for mumbai

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: "https://matic-mumbai.chainstacklabs.com",
+      chainId: 80001,
+      accounts:
+        process.env.PRIVATE_KEY_MUMBAI !== undefined
+          ? [process.env.PRIVATE_KEY_MUMBAI]
+          : [],
+    },
+  },
 };
 
 export default config;
--- smart-contracts/scripts/deploy.ts
+++ smart-contracts/scripts/deploy.ts
@@ -1,10 +1,15 @@
 import { ethers } from "hardhat";
+import { BigNumberish } from "ethers";
 
-type DeployArgs = [_tokenSupply: string, _maxSupply: string, _baseURI: string];
+type DeployArgs = [
+  _tokenSupply: BigNumberish,
+  _maxSupply: BigNumberish,
+  _baseURI: string
+];
 
 const deploy = async () => {
   const deployArgs: DeployArgs = [
-    "0.01",
+    ethers.utils.parseEther("0.01"),
     "6",
     "ipfs://QmesdkpFTZF96SEa8ppPCkP2aibiE9yqJ6WBPi5XLjb6kS/",
   ];
